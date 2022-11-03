f96b5cf6931f4f3c620c587ceb65b7bf4a457656
Léo Brossard <leo_brossard@hotmail.fr>
2022-11-03 20:49:46 +0100

    fixed another error

--- config.lua
+++ config.lua
@@ -212,7 +212,7 @@ Config.CraftingItems = {
     }, ]]
 }
 
---Config.AttachmentCraftingLocation = vector3(88.91, 3743.88, 40.77)
+Config.AttachmentCraftingLocation = vector3(88.91, 9999999, 40.77)
 
 Config.AttachmentCrafting = {
    --[[  ["items"] = {
